aba17b7ca26c0666105ce21a6840061098695dbb
gustaff122 <xseoos@gmail.com>
2023-04-27 22:04:57 +0200

    mainpage and series views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from '@kakkoii/ui/organisms/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { cacheInterceptorProvider } from '@kakkoii/interceptors/cache.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,12 @@ import { cacheInterceptorProvider } from '@kakkoii/interceptors/cache.intercepto
     HttpClientModule,
     AppRoutingModule,
     NavbarComponent,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     cacheInterceptorProvider
