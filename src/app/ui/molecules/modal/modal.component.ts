44daa16572ecdcbf6d83d1bd4f1f52887d6e25d6
blazej.klopotek <bklopotek@9bits.pl>
2023-10-31 17:18:23 +0100

    add modal

--- src/app/ui/molecules/modal/modal.component.ts
+++ src/app/ui/molecules/modal/modal.component.ts
@@ -1,13 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgIconComponent } from '@ng-icons/core';
+import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'kk-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, NgIconComponent ],
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModalComponent {
+  @Input() public title: string = '';
+
+  constructor(
+    private readonly dialogRef: DialogRef
+  ) {
+  }
+
+  public close(): void {
+    this.dialogRef.close()
+  }
 }
