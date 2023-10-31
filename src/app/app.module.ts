841c2a8209b5ce56a79b91158c0ff130962101f4
blazej.klopotek <bklopotek@9bits.pl>
2023-10-31 17:18:23 +0100

    add modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,45 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { FooterComponent } from '@kakkoii/ui/organisms/footer/footer.component';
 import { NgIconsModule, provideIcons } from '@ng-icons/core';
-import { matExpandMore, matHome, matSearch, matWifiOff, matArrowDropUp, matArrowDropDown, matTune, matPlayArrow, matNavigateNext, matNavigateBefore } from '@ng-icons/material-icons/baseline';
+import { matExpandMore, matHome, matSearch, matWifiOff, matArrowDropUp, matArrowDropDown, matTune, matPlayArrow, matNavigateNext, matNavigateBefore, matClose } from '@ng-icons/material-icons/baseline';
 import { matInfoOutline, matSavingsOutline } from '@ng-icons/material-icons/outline';
 import { bootstrapTwitter, bootstrapDiscord, bootstrapFacebook, bootstrapXCircle } from '@ng-icons/bootstrap-icons';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgProgressModule } from 'ngx-progressbar';
+import { DEFAULT_DIALOG_CONFIG } from '@angular/cdk/dialog';
+
+const components = [
+  NavbarComponent,
+  FooterComponent,
+  NgIconsModule,
+  NgProgressModule.withConfig({
+    spinner: false,
+    color: '#3b5eda',
+    speed: 300,
+    trickleSpeed: 900,
+  }),
+]
+
+const icons = [ provideIcons({
+    matExpandMore,
+    matHome,
+    matSearch,
+    matInfoOutline,
+    matSavingsOutline,
+    bootstrapTwitter,
+    bootstrapDiscord,
+    bootstrapXCircle,
+    bootstrapFacebook,
+    matWifiOff,
+    matArrowDropUp,
+    matArrowDropDown,
+    matPlayArrow,
+    matTune,
+    matNavigateNext,
+    matNavigateBefore,
+    matClose
+  })
+]
 
 @NgModule({
   declarations: [
@@ -23,43 +57,20 @@ import { NgProgressModule } from 'ngx-progressbar';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    NavbarComponent,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    FooterComponent,
-    NgIconsModule,
-    NgProgressModule.withConfig({
-      spinner: false,
-      color: '#3b5eda',
-      speed: 300,
-      trickleSpeed: 900,
-    }),
+    ...components
   ],
   providers: [
-    provideIcons({
-      matExpandMore,
-      matHome,
-      matSearch,
-      matInfoOutline,
-      matSavingsOutline,
-      bootstrapTwitter,
-      bootstrapDiscord,
-      bootstrapXCircle,
-      bootstrapFacebook,
-      matWifiOff,
-      matArrowDropUp,
-      matArrowDropDown,
-      matPlayArrow,
-      matTune,
-      matNavigateNext,
-      matNavigateBefore,
-    }),
+    ...icons,
+    {
+      provide: DEFAULT_DIALOG_CONFIG, useValue: { panelClass: 'modal'}
+    }
   ],
   bootstrap: [ AppComponent ],
 })
+
 export class AppModule {
 }
--- src/app/core/auth/modals/register-modal/register-modal.component.ts
+++ src/app/core/auth/modals/register-modal/register-modal.component.ts
@@ -1,15 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '@kakkoii/ui/molecules/modal/modal.component';
+import { StepperComponent } from '@kakkoii/ui/molecules/stepper/stepper.component';
+import { StepperItem } from '@kakkoii/ui/molecules/stepper/models/stepper-item';
 
 @Component({
   selector: 'kk-register-modal',
   standalone: true,
-  imports: [ CommonModule, ModalComponent ],
+  imports: [ CommonModule, ModalComponent, StepperComponent ],
   templateUrl: './register-modal.component.html',
   styleUrls: ['./register-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterModalComponent {
-
+  public readonly steps: StepperItem[] = [
+    {
+      active: true,
+      title: 'Rejestracja',
+    },
+    {
+      active: false,
+      title: 'Potwierdzenie maila'
+    },
+    {
+      active: false,
+      title: 'Konfiguracja konta'
+    }
+  ]
 }
